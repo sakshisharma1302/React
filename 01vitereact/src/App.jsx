989a4b291ddcc1967bbca36c301255ee47378c92
Sakshi Sharma <sakshisharmaics17@gmail.com>
2024-08-04 14:24:44 +0530

    initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import Chai from "./chai";
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <Chai/>
+  <h1> chai aur react</h1>
+  <p>test para</p>
+  </>
+      
+  );
+}
+
+export default App
